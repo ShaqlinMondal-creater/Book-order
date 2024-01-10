f07ec624f05ce21ef50757e7d076ab7d5429aed6
Shaqlin Mondal <79474217+ShaqlinMondal-creater@users.noreply.github.com>
2024-01-10 17:29:38 +0530

    Add files via upload

---
+++ UI/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  cloudinaryconfig:({ 
+    cloud_name: 'dr0ezjcyy', 
+    api_key: '147367327242121', 
+    api_secret: 'XfJi88Gq_7nrmuSQ6gsAWENzaTY',
+    upload_preset: 'UUUUUUUU',
+    secure: true
+  })
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
